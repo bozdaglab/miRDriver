ebee1de0768ee1c2bee7cc3fb4edef3aaf9fb31b
mmor0728 <matt.moravec222@gmail.com>
2020-06-26 13:57:29 -0500

    commit
    Former-commit-id: cd5e1b5ceb9e438b77d27d834994524f3d43eaa1 [formerly 18d5b41e2ab1cedc2b91aed826db0808ba8bbee5] [formerly 18d5b41e2ab1cedc2b91aed826db0808ba8bbee5 [formerly 18d5b41e2ab1cedc2b91aed826db0808ba8bbee5 [formerly 18d5b41e2ab1cedc2b91aed826db0808ba8bbee5 [formerly 18d5b41e2ab1cedc2b91aed826db0808ba8bbee5 [formerly fbd690872ed019ae32fd744738b4229a5abacc63 [formerly 25aa02ded970bfba4278429d3d1a5eed0c84ef75] [formerly 365e2fb6bbc3708b8fa05006c34491b8c90f458b] [formerly 6e511c6f77da6c6300c4f4bb1be659aa582b50b4]]]]]]
    Former-commit-id: fbd690872ed019ae32fd744738b4229a5abacc63 [formerly 25aa02ded970bfba4278429d3d1a5eed0c84ef75] [formerly 365e2fb6bbc3708b8fa05006c34491b8c90f458b]
    Former-commit-id: cd71773e3129a362c9fe45d506a53d9dd5baa9d4
    Former-commit-id: 2b561c886d0e7cd7766913af0b10fe000b6b4a93
    Former-commit-id: 3d647fa395eb1591439eb1d3c94fa1530dca9101
    Former-commit-id: dec29bcb45e1d34a9f5d97d2d9b035c1e2a4892f [formerly 5e22670a8c80bc74905d0d6f406b2d3f575cd3e0 [formerly 838230b7a63c6d793f2af3e54db5ceea92f199de] [formerly b53bbee510736defbfbc5b77763881cfd8f904a2] [formerly 8eaaf8f616336639e71927100cd838aa7681ba7e]]
    Former-commit-id: 7d6b61f753bcf737b98050c07ac68c1df5ef7663
    Former-commit-id: a233e3502e315af11770f846e98569fe893a97b4
    Former-commit-id: 8083eece1653585bd55650e0725d42bf7e82ce3d
    Former-commit-id: 56915faf46dff05a037409f4ce1be8cdb0ba01d5

---
+++ R/lassoParallelForTransGene_FINAL_03.R
@@ -0,0 +1,205 @@
+## miRDriver STEP 2
+## Code 6
+## Created By: Banabithi Bose
+## Date Created: .........
+lassoParallelForTransGene <-
+  function(ncores = 2,
+           numCounter = 100,
+           Nfolds = 10,
+           nonZeroPercent = 70 ,
+           parallel = c('TRUE', 'FALSE')) {
+    dir.create("~/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff")
+
+    processLassoCtr <- function(numCounter, Lx, Ly, gene_id) {
+      tryCatch({
+        cvfit = cv.glmnet(
+          Lx,
+          Ly,
+          alpha = 1,
+          standardize = TRUE,
+          standardize.response = FALSE,
+          nfolds = Nfolds
+        ) #main regression
+        #code below is for creating the dataframe output
+        Coeff <- data.frame("", "", "")
+        colnames(Coeff) <-
+          c("coefficients_min", "genes_min", "predictors_min")
+        Coeff_N <- data.frame("", "", "")
+        colnames(Coeff_N) <-
+          c("coefficients_1se", "genes_1se", "predictors_1se")
+        #time<-system.time(cvfit) #didn't work
+        coef_data <- coef(cvfit, s = "lambda.min")[-1, ]
+        x <- data.frame(coef_data)
+        x$Gene_ID <- gene_id
+        x$Predictors <- row.names(x)
+        x <- data.table(x)
+        colnames(x) <-
+          c("coefficients_min", "genes_min", "predictors_min")
+        Coeff <- rbind(Coeff, x)
+        Coeff <- Coeff[-1, ]
+        coef_data_N <- coef(cvfit, s = "lambda.1se")[-1, ]
+        N <- data.frame(coef_data_N)
+        N$Gene_ID <- gene_id
+        N$Predictors <- row.names(N)
+        N <- data.table(N)
+        colnames(N) <-
+          c("coefficients_1se", "genes_1se", "predictors_1se")
+        Coeff_N <- rbind(Coeff_N, N)
+        Coeff_N <- Coeff_N[-1, ]
+        Tot_Coeff <- data.frame(Coeff, Coeff_N)
+        return(Tot_Coeff)
+      }, error = function(error_condition) {
+        cat(
+          paste0(gene_id, " : ", error_condition),
+          file = "~/mirDriverFold/ERROR_FILES/Step4_LassoErrors.txt",
+          sep = "\n",
+          append = TRUE
+        )
+        return()
+      }, finally = {
+
+      })
+      return()
+    }
+
+    processUpDownGeneFiles <- function(fileName, processLassoCtr) {
+      load(paste0(
+        "~/mirDriverFold/miRDriver_Step4/TransGenePredFile/",
+        fileName,
+        sep = ""
+      ))
+      gene_id <- str_sub(fileName, length(fileName), -5)
+      if (colnames(GenePred)[1] == "sample")
+      {
+        GenePred <- GenePred[, -1]
+      } else
+      {
+        GenePred <- GenePred
+      }
+
+      GenePred <-
+        subset(GenePred, select = which(!duplicated(names(GenePred))))##checking on duplicated colnames
+      if (ncol(GenePred) > 2)
+      {
+        Lx <- data.matrix(GenePred[, -1, drop = TRUE])
+        Ly <- data.matrix(GenePred[, 1])
+
+        j <- 1:numCounter
+
+        Tot_Coeff_2 <- lapply(j, processLassoCtr, Lx, Ly, gene_id)
+        Tot_Coeff <- rbindlist(Tot_Coeff_2, use.names = TRUE, fill = TRUE)
+        min_Coeff <- Tot_Coeff[, 1:3]
+        fse_Coeff <- Tot_Coeff[, 4:6]
+        save(
+          min_Coeff,
+          file = paste0(
+            "~/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff/",
+            gene_id,
+            ".Rda"
+          )
+        )
+
+      } else
+      {
+        save(GenePred,
+             file = paste0(
+               "~/mirDriverFold/ERROR_FILES/Step4_Lasso_",
+               gene_id,
+               ".Rda"
+             ))##this will give us genes on which lasso didn't run
+      }
+
+      rm(list = ls())
+      gc()
+    }
+
+    #################
+    # STEP 1: Make parallel cluster and export the variables to all the clusters
+    #################
+
+    if (parallel == TRUE) {
+      print("cluster making started")
+      cl <-
+        makeCluster(mc <- getOption("cl.cores", ncores), type = "FORK")
+      invisible(clusterEvalQ(cl, library(glmnet)))
+      invisible(clusterEvalQ(cl, library(janitor)))
+      invisible(clusterEvalQ(cl, library(stringr)))
+      invisible(clusterEvalQ(cl, library(plyr)))
+      invisible(clusterEvalQ(cl, library(data.table)))
+      invisible(clusterEvalQ(cl, library(readr)))
+      invisible(clusterEvalQ(cl, library(gsubfn)))
+      invisible(clusterEvalQ(cl, library(parallel)))
+      invisible(clusterEvalQ(cl, library(sqldf)))
+      invisible(clusterEvalQ(cl, library(reshape)))
+      invisible(clusterEvalQ(cl, library(reshape2)))
+      invisible(clusterEvalQ(cl, library(pbapply)))
+
+      print("cluster export started")
+      parallel::clusterExport(
+        cl = cl,
+        c("numCounter", "Nfolds", "processLassoCtr"),
+        envir = environment()
+      )#envir needed to be correct, export from global aswell as local
+      print("cluster making done")
+
+
+      fileNames <-
+        list.files("~/mirDriverFold/miRDriver_Step4/TransGenePredFile/",
+                   pattern = "*.Rda") ##we have 3518 GenePred files in this folder
+      parLapplyLB(cl, fileNames, processUpDownGeneFiles, processLassoCtr)
+      stopCluster(cl)
+      print("cluster stopped")
+    }
+
+    if (parallel == FALSE) {
+      fileNames <-
+        list.files("~/mirDriverFold/miRDriver_Step4/TransGenePredFile/",
+                   pattern = "*.Rda") ##we have 3518 GenePred files in this folder
+      lapply(fileNames, processUpDownGeneFiles, processLassoCtr)
+
+    }
+    ##########End Of Code###############
+
+    processMirGene <- function(fileNames, nonZeroTimes) {
+      load(paste0(
+        "~/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff/",
+        fileNames
+      ))
+      gene_id <- str_sub(fileNames, end = -5)
+      nx <-
+        sqldf(
+          "select genes_min,predictors_min,count(coefficients_min) coeff_min_count, avg(coefficients_min)
+          avg_coeff from min_Coeff where coefficients_min <> 0 group by genes_min,predictors_min "
+        )
+
+      selected_features <-
+        sqldf(paste0("select * from nx where coeff_min_count >=", nonZeroTimes))
+      if (nrow(selected_features) > 0)
+      {
+        selected_features$genes_min <- gene_id
+        gene_with_mirana <-
+          sqldf("select * from selected_features where predictors_min like '%hsa%'")
+        d1 <- data.table(gene_with_mirana[, -3])
+        d1 <- unique(d1)
+        colnames(d1) <- c("genes", "miRNA", "av_coeff")
+        Mir_Gen <- data.table(d1)
+        return(Mir_Gen)
+      }
+      else
+      {
+        print("this file doesn't have any mirnas as predictors")
+        return()
+
+      }
+
+    }
+    nonZeroTimes <- round(numCounter * (nonZeroPercent / 100))
+    LASSOfileNames <-
+      list.files("~/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff/", pattern = "*.Rda")#3117 genes
+    Final_gene_mirna_network <-
+      rbindlist(lapply(LASSOfileNames, processMirGene, nonZeroTimes))
+    Final_gene_mirna_network <- unique(Final_gene_mirna_network)
+    save(Final_gene_mirna_network, file = "~/mirDriverFold/miRDriver_RESULTS/Final_gene_mirna_network.Rda")
+
+    return(Final_gene_mirna_network)
+  }
