f7b512226f7a7ea1eeed74ea5eb0f429bd99431e
Matthew Moravec <matt.moravec222@gmail.com>
2020-12-11 15:52:28 -0600

    c
    Former-commit-id: fc8a09a5139f11969e4c077255d040524c96bb0b

---
+++ R/lassoParallelForTransGene_FINAL_03.R
@@ -0,0 +1,132 @@
+## miRDriver STEP 4
+## Code 2
+## Created By: Banabithi Bose
+## Date Created: 5/1/2019
+lassoParallelForTransGene <-function(ncores = 2,numCounter = 100,Nfolds = 10,nonZeroPercent = 70,parallel = c('TRUE', 'FALSE'), mirdirectory = "~/") {
+    dir.create(paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff"))
+    processLassoCtr <- function(numCounter, Lx, Ly, gene_id) {
+        tryCatch({
+            #cvfit = cv.glmnet(Lx,Ly,alpha = 1,standardize = TRUE,standardize.response = FALSE,nfolds = Nfolds)
+            
+            cvfit = suppressWarnings(cv.glmnet(Lx,Ly,alpha = 1,standardize = TRUE,standardize.response = FALSE,nfolds = Nfolds))
+            #code below is for creating the dataframe output
+            Coeff <- data.frame("", "", "")
+            colnames(Coeff) <-c("coefficients_min", "genes_min", "predictors_min")
+            Coeff_N <- data.frame("", "", "")
+            colnames(Coeff_N) <-c("coefficients_1se", "genes_1se", "predictors_1se")
+            coef_data <- coef(cvfit, s = "lambda.min")[-1, ]
+            x <- data.frame(coef_data)
+            x$Gene_ID <- gene_id
+            x$Predictors <- row.names(x)
+            x <- data.table(x)
+            colnames(x) <-c("coefficients_min", "genes_min", "predictors_min")
+            Coeff <- rbind(Coeff, x)
+            Coeff <- Coeff[-1, ]
+            coef_data_N <- coef(cvfit, s = "lambda.1se")[-1, ]
+            N <- data.frame(coef_data_N)
+            N$Gene_ID <- gene_id
+            N$Predictors <- row.names(N)
+            N <- data.table(N)
+            colnames(N) <-c("coefficients_1se", "genes_1se", "predictors_1se")
+            Coeff_N <- rbind(Coeff_N, N)
+            Coeff_N <- Coeff_N[-1, ]
+            Tot_Coeff <- data.frame(Coeff, Coeff_N)
+            return(Tot_Coeff)
+        }, error = function(error_condition) {
+            cat(paste0(gene_id, " : ", error_condition),file = paste0(mirdirectory,"/mirDriverFold/ERROR_FILES/Step4_LassoErrors.txt"),sep = "\n",append = TRUE)
+            return()
+        }, finally = {
+        })
+        return()
+    }
+    processUpDownGeneFiles <- function(fileName, processLassoCtr) {
+        load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step4/TransGenePredFile/",fileName,sep = ""))
+        gene_id <- str_sub(fileName, length(fileName), -5)
+        if (colnames(GenePred)[1] == "sample")
+        {
+            GenePred <- GenePred[, -1]
+        } else
+        {
+            GenePred <- GenePred
+        }
+        GenePred <-subset(GenePred, select = which(!duplicated(names(GenePred))))
+        if (ncol(GenePred) > 2)
+        {
+            Lx <- data.matrix(GenePred[, -1, drop = TRUE])
+            Ly <- data.matrix(GenePred[, 1])
+            j <- seq_len(numCounter)
+            Tot_Coeff_2 <- lapply(j, processLassoCtr, Lx, Ly, gene_id)
+            Tot_Coeff <- rbindlist(Tot_Coeff_2, use.names = TRUE, fill = TRUE)
+            min_Coeff <- Tot_Coeff[, c(1,2,3)]
+            fse_Coeff <- Tot_Coeff[, c(4,5,6)]
+            save(min_Coeff,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff/",gene_id,".Rda"))
+        } else
+        {
+            save(GenePred,file = paste0(mirdirectory,"/mirDriverFold/ERROR_FILES/Step4_Lasso_",gene_id,".Rda"))
+        }
+        rm(list = ls())
+        gc()
+    }
+    #################
+    ## STEP 1: Make parallel cluster and export the variables to all the clusters
+    #################
+    if (parallel == TRUE) {
+        #print("cluster making started")
+        if(.Platform$OS.type == "windows"){
+            cl <- makeCluster(mc <- getOption("cl.cores", ncores), type = "PSOCK")
+        }else{
+            cl <- makeCluster(mc <- getOption("cl.cores", ncores), type = "FORK")
+        }
+        invisible(clusterEvalQ(cl, library(glmnet)))
+        invisible(clusterEvalQ(cl, library(janitor)))
+        invisible(clusterEvalQ(cl, library(stringr)))
+        invisible(clusterEvalQ(cl, library(plyr)))
+        invisible(clusterEvalQ(cl, library(data.table)))
+        invisible(clusterEvalQ(cl, library(readr)))
+        invisible(clusterEvalQ(cl, library(gsubfn)))
+        invisible(clusterEvalQ(cl, library(parallel)))
+        invisible(clusterEvalQ(cl, library(sqldf)))
+        invisible(clusterEvalQ(cl, library(reshape)))
+        invisible(clusterEvalQ(cl, library(reshape2)))
+        invisible(clusterEvalQ(cl, library(pbapply)))
+        #print("cluster export started")
+        parallel::clusterExport(cl = cl,c("numCounter", "Nfolds", "processLassoCtr"),envir = environment())
+        #print("cluster making done")
+        fileNames <-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step4/TransGenePredFile/"),pattern = "*.Rda") 
+        parLapplyLB(cl, fileNames, processUpDownGeneFiles, processLassoCtr)
+        stopCluster(cl)
+        #print("cluster stopped")
+    }
+    if (parallel == FALSE) {
+        fileNames <-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step4/TransGenePredFile/"),pattern = "*.Rda")
+        lapply(fileNames, processUpDownGeneFiles, processLassoCtr)
+    }
+    ##########End Of Code###############
+    processMirGene <- function(fileNames, nonZeroTimes) {
+        load(paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff/",fileNames))
+        gene_id <- str_sub(fileNames, end = -5)
+        nx <-sqldf("select genes_min,predictors_min,count(coefficients_min) coeff_min_count, avg(coefficients_min)avg_coeff from min_Coeff where coefficients_min <> 0 group by genes_min,predictors_min ")
+        selected_features <-sqldf(paste0("select * from nx where coeff_min_count >=", nonZeroTimes))
+        if (nrow(selected_features) > 0)
+        {
+            selected_features$genes_min <- gene_id
+            gene_with_mirana <-sqldf("select * from selected_features where predictors_min like '%hsa%'")
+            d1 <- data.table(gene_with_mirana[, -3])
+            d1 <- unique(d1)
+            colnames(d1) <- c("genes", "miRNA", "av_coeff")
+            Mir_Gen <- data.table(d1)
+            return(Mir_Gen)
+        }else
+        {
+        #print("this file doesn't have any mirnas as predictors")
+        return()
+        }
+    }
+    nonZeroTimes <- round(numCounter * (nonZeroPercent / 100))
+    LASSOfileNames <-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS/LassoMinCoeff/"), pattern = "*.Rda")
+    Final_gene_mirna_network <-rbindlist(lapply(LASSOfileNames, processMirGene, nonZeroTimes))
+    Final_gene_mirna_network <- unique(Final_gene_mirna_network)
+    save(Final_gene_mirna_network, file = paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS/Final_gene_mirna_network.Rda"))
+    return(Final_gene_mirna_network)
+}
+
---
+++ R/makingCisAndTransGenes_FINAL_03.R
@@ -0,0 +1,153 @@
+## miRDriver STEP 2
+## Code 2
+## Created By: Banabithi Bose
+## Date Created: 5/1/2019
+makingCisAndTransGenes <-function(Genes_coord_bedfile, gistic_bedfile, mirdirectory="~/") {
+    if ((is(Genes_coord_bedfile, "data.frame")) || (is(Genes_coord_bedfile,"matrix"))){
+        Genes_coord_bedfile<-Genes_coord_bedfile
+    }else if (is(Genes_coord_bedfile,"SummarizedExperiment")){
+        Genes_coord_bedfile<-assay(Genes_coord_bedfile)
+    }else{
+        #print("")
+    }
+
+    if ((is(gistic_bedfile, "data.frame")) || (is(gistic_bedfile,"matrix"))){
+        gistic_bedfile<-gistic_bedfile
+    }else if (is(gistic_bedfile,"SummarizedExperiment")){
+        gistic_bedfile<-assay(gistic_bedfile)
+    }else{
+        #print("")
+    }
+
+
+    ##making necessary directories
+    dir.create(paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS"))
+    dir.create(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES"))
+    dir.create(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files"))
+    ############## Reading up/down genes files for a gistic region
+    fileNames <-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/UpDownGeneLibrary/"),pattern = "*.txt")
+    Count_Cis_Trans <- data.frame("", "", "")
+    colnames(Count_Cis_Trans) <-c("Gistic_Region", "cis_genes_count", "trans_genes_count")
+    i <- as.integer(seq_along(fileNames))
+    MakecistransFile <- function(i) {
+        #print(i)
+        upgene <-read.table(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/UpDownGeneLibrary/",fileNames[i]),sep = " ",header = TRUE)
+        genes <- data.frame(upgene$Row.Names)
+        colnames(genes) <- "gene_id"
+        Gistic_Region1 <- gsub(".*_", "", fileNames[i])
+        Gistic_Region <-str_sub(Gistic_Region1, 1, str_length(Gistic_Region1) - 4)
+        if ((gistic_bedfile == FALSE)[1] ){
+            load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step1/GisticResults/gistic_bedfile.rda"))
+            region$chr <- gsub("[chr]", "", region$chr)
+            Region = region[region$peak == Gistic_Region, ]
+        } else{
+            Region = gistic_bedfile[gistic_bedfile$peak == Gistic_Region, ]
+        }
+        if (nrow(Region) == 0) {
+            #print("this gistic region is not valid")
+        } else {
+            Region <- bedr.sort.region(Region, check.chr = FALSE, verbose = FALSE)
+            Gistic_Genes <-bedr.join.region(Genes_coord_bedfile,Region,report.n.overlap = TRUE,check.chr = FALSE, verbose = FALSE)
+            cis_genes <-data.frame(Gistic_Genes$gene)
+            if (nrow(cis_genes) == 0) {
+                #print("This region has no cis genes")
+                trans_genes <- genes
+                colnames(trans_genes) <- "gene_id"
+                save(trans_genes,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/trans_",fileNames[i],".Rda"))
+            } else{
+                colnames(cis_genes) <- "gene_id"
+                save(cis_genes,file = paste0("~/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/cis_",fileNames[i],".Rda"))
+                trans_genes <- data.frame(genes[-c(cis_genes$gene_id), ])
+                colnames(trans_genes) <- "gene_id"
+                save(trans_genes,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/trans_",fileNames[i],".Rda"))
+            }
+        Count_Cis_Trans1 <-data.table(cbind(fileNames[i], nrow(unique(cis_genes)), nrow(unique(trans_genes))))
+        colnames(Count_Cis_Trans1) <-c("Gistic_Region","cis_genes_count","trans_genes_count")
+        return(Count_Cis_Trans1)
+        }
+    }   
+    Count_Cis_Trans <- rbindlist(lapply(i, MakecistransFile))
+    Count_Cis_Trans <- Count_Cis_Trans[-1, ]
+    Count_Cis_Trans <- unique(Count_Cis_Trans)
+    save(Count_Cis_Trans, file = paste0(mirdirectory,"/mirDriverFold/miRDriver_RESULTS/Count_Cis_Trans.Rda"))
+    r <- Count_Cis_Trans[Count_Cis_Trans$trans_genes_count == 0, ]
+    #### Removing regions from CIS_TRANS_FILES where there are no trans genes
+    fileNames1 <-list.files("~/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/", pattern = "*.Rda")
+    fileNames2 <- r$Gistic_Region
+    for (i in seq_along(fileNames1)){
+        fileNamesD <- gsub("(cis_)", "", fileNames1[i])
+        fileNamesD <- gsub("(trans_)", "", fileNamesD)
+        fileNamesD <- str_sub(fileNamesD, end = -5)
+        if (is.element(fileNamesD, fileNames2)) {
+            file.remove(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/",fileNames1[i]))
+        } else
+        {
+        }
+    }
+    ##Merging script portion of the function
+    processUpDownGeneFiles <- function(fileNameUpDn) {
+        trans_genes <- NULL
+        RegionNameOnly <-str_sub(gsub(".*_", "", fileNameUpDn), length(fileNameUpDn), -9)
+        if (str_sub(fileNameUpDn, 1, 3) == "cis") {
+            load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/",fileNameUpDn))
+            load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/",gsub(".*cis", "trans", fileNameUpDn)))
+        } else if (str_sub(fileNameUpDn, 1, 5) == "trans")
+        {
+            load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/",fileNameUpDn))
+            cis_file_name <- paste0(gsub(".*trans", "cis", fileNameUpDn))
+            if (is.element(cis_file_name, fileNames))
+            {
+                load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES/",gsub(".*trans", "cis", fileNameUpDn)))
+            } else
+            {
+                cis_genes <- data.frame("")
+                colnames(cis_genes) <- "gene_id"
+            }
+        }
+        transfile <- str_sub(fileNameUpDn, -80, -9)
+        transfile <- gsub("cis_", "trans_", transfile)
+        if(!dir.exists(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",RegionNameOnly))){
+            dir.create(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",RegionNameOnly))
+        }
+        save(cis_genes,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",RegionNameOnly,"/cis_genes.Rda"))
+        save(trans_genes,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",RegionNameOnly,"/",transfile,".Rda"))
+    }
+    ##### MAIN PROGRAM ############################################################
+    fileNames <-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/CIS_TRANS_FILES"), pattern = "*.Rda")
+    lapply(fileNames, processUpDownGeneFiles)
+    ################### END #######################################################
+    ## MERGING trans_UPgene and trans_DOWNgene data
+    processUpDownGeneFiles2 <- function(fileName) {
+        trans_gene <- NULL
+        genefiles <- NULL
+        genefiles<-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",fileName,"/"))
+        transfiles <- paste0("trans_DOWNgene_", fileName, ".Rda")
+        transfiles_new <- paste0("trans_UPgene_", fileName, ".Rda")
+        if (is.element(transfiles, genefiles)) {
+            load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",fileName,"/",transfiles))
+            transgene1 <- trans_genes
+            colnames(transgene1) <- "gene_id"
+            if (is.element(transfiles_new, genefiles)) {
+                load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",fileName,"/",transfiles_new))
+                transgene2 <- trans_genes
+                colnames(transgene2) <- "gene_id"
+            } else {
+                transgene2 <- data.frame("")
+                colnames(transgene2) <- "gene_id"
+            }
+            trans_genes <- rbind(transgene1, transgene2)
+            save(trans_genes,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",fileName,"/trans_genes.Rda"))
+        } else{
+            load(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",fileName,"/",transfiles_new))
+            transgene2 <- trans_genes
+            transgene1 <- data.frame("")
+            colnames(transgene2) <- "gene_id"
+            colnames(transgene1) <- "gene_id"
+            trans_genes <- rbind(transgene1, transgene2)
+            save(trans_genes,file = paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/",fileName,"/trans_genes.Rda"))
+        }
+    }
+    folderNames <-list.files(paste0(mirdirectory,"/mirDriverFold/miRDriver_Step2/Trans_Cis_Files/"))
+    lapply(folderNames, processUpDownGeneFiles2)
+    return(paste0("Trans genes and cis genes files produced in folder mirDriverFold/miRDriver_Step2/Trans_Cis_Files"))
+}
